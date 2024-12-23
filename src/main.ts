9ded44744d7caf14e35564de566992676d495360
polo372 <plbd@laposte.net>
2024-12-23 20:38:50 +0100

    label main.ts

--- src/main.ts
+++ src/main.ts
@@ -8,3 +8,11 @@ document.getElementById("banner")!.innerHTML = `
 document
   .getElementById("startGame")
   ?.addEventListener("click", () => startGame());
+
+<label for="town-select">Choisis ton secteur</label>
+
+<select name="town" id="town-select">
+  <option value="">--Merci de choisir votre secteur--</option>
+  <option value="Ouest Agglo">Ouest Agglo</option>
+  <option value="Tours centre">Tours Centre</option>
+</select>
