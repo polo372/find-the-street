699824368708439f711f33bfe461dd91c7df5fd7
polo372 <plbd@laposte.net>
2024-12-23 20:43:42 +0100

    modif main `label`

--- src/main.ts
+++ src/main.ts
@@ -3,16 +3,17 @@ import "./style.css";
 
 document.getElementById("banner")!.innerHTML = `
   <button id="startGame">Démarrer le jeu</button>
+  <label for="town-select">Choisis ton secteur</label>
+  <select name="town" id="town-select">
+    <option value="">--Merci de choisir votre secteur--</option>
+    <option value="Ouest Agglo">Ouest Agglo</option>
+    <option value="Tours centre">Tours Centre</option>
+  </select>
 `;
 
 document
   .getElementById("startGame")
   ?.addEventListener("click", () => startGame());
 
-<label for="town-select">Choisis ton secteur</label>
+  
 
-<select name="town" id="town-select">
-  <option value="">--Merci de choisir votre secteur--</option>
-  <option value="Ouest Agglo">Ouest Agglo</option>
-  <option value="Tours centre">Tours Centre</option>
-</select>
